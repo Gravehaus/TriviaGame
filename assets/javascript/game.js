8692f26c8e8d19fab2c6fd67f1588145263a3043
Julia graves <gravehaus@gmail.com>
2019-09-06 16:49:15 -0700

    editing js

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//initial values
+let counter = 10;
+let currentQuestion = 0;
+let score =0;
+let lost = 0;
+let timer;
+
+//Display question and choices in browser
+
+
+function loadQuestion(){
+    const question= quizQuestions[currentQuestion].question;
+    const choices= quizQuestions[currentQuestion].choices;
+
+    $('#game').html('<h4>' + question + '</h4>');
+
+}
