a874260774e161ae08a35a987a385d86b9206bad
Julia graves <gravehaus@gmail.com>
2019-09-06 17:36:56 -0700

    Edited Quiz Questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,25 @@
-const quizQuestions = [
-    {
-        question : "In the comics, who is Red's starting Pokemon?", //pokemon
-        imgSrc : "../images/pikachu.gif",
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "What game did Mario first appear in?", //mario
-        imgSrc : "../images/mario.gif",
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "How many timelines are in the Legend of Zelda series?", //loz
-        imgSrc : "../images/link-loz.gif",
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "Who is the current Lich King?", //wow
-        imgSrc : "../images/lichking.gif",
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "Who is the protagonist of the Mass Effect trilogy?", //mass effect
-        imgSrc : "../images/mass-effectgif.gif",
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "What US city does Deus Ex:Humant Revolution start in?", //de:hr
-        imgSrc : "../images/adamjensen.gif",
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "",
-        imgSrc : "../images/cloud-seph.gif", //final fantasy
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "",
-        imgSrc : "../images/smash-bros.gif", //smash bros
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "",
-        imgSrc : "../images/persona-5.gif", //persona
-        choices: [],
-        correctAnswer: 4
-    },
-    {
-        question : "",
-        imgSrc : "../images/robbie-silenthill.gif", //silent hill
-        choices: [],
-        correctAnswer: 4
-    },
-    
-];
+//initial values
+let counter = 10;
+let currentQuestion = 0;
+let score =0;
+let lost = 0;
+let timer;
+
+//Display question and choices in browser
+
+
+function loadQuestion(){
+    const question= quizQuestions[currentQuestion].question;
+    const choices= quizQuestions[currentQuestion].choices;
+
+    $('#game').html('<h4>' + question + '</h4>');
+
+}
+
+loadQuestion();
+
+
+
+
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,63 @@
-//initial values
-let counter = 10;
-let currentQuestion = 0;
-let score =0;
-let lost = 0;
-let timer;
-
-//Display question and choices in browser
-
-
-function loadQuestion(){
-    const question= quizQuestions[currentQuestion].question;
-    const choices= quizQuestions[currentQuestion].choices;
-
-    $('#game').html('<h4>' + question + '</h4>');
-
-}
-
-loadQuestion();
+const quizQuestions = [
+    {
+        question : "In the comics, who is Red's starting Pokemon?", //pokemon
+        imgSrc : "../images/pikachu.gif",
+        choices: ["Clefairy","Pikachu","Bulbasaur","Eevee"],
+        correctAnswer: "Clefairy"
+    },
+    {
+        question : "What game did Mario first appear in?", //mario
+        imgSrc : "../images/mario.gif",
+        choices: ["Super Mario Bros","Donkey Kong","Legend of Zelda", "Pong"],
+        correctAnswer: "Donkey Kong"
+    },
+    {
+        question : "How many timelines are in the Legend of Zelda series?", //loz
+        imgSrc : "../images/link-loz.gif",
+        choices: ["Two", "Three", "Four", "One"],
+        correctAnswer: "Three"
+    },
+    {
+        question : "Who is the current Lich King?", //wow
+        imgSrc : "../images/lichking.gif",
+        choices: ["Arthas Menethil", "Bolvar Fordragon","Sylvanas Windrunner","Thrall"],
+        correctAnswer: "Bolvar Fordragon"
+    },
+    {
+        question : "Who is the protagonist of the Mass Effect trilogy?", //mass effect
+        imgSrc : "../images/mass-effectgif.gif",
+        choices: ["Captain Shepard","Officer Shepard","John Shepard","Commander Shepard"],
+        correctAnswer:"Commander Shepard"
+    },
+    {
+        question : "What US city does Deus Ex:Humant Revolution start in?", //de:hr
+        imgSrc : "../images/adamjensen.gif",
+        choices: ["Hong Kong","Detroit","New York","Seattle"],
+        correctAnswer: "Seattle"
+    },
+    {
+        question : "Which Final Fantasy character has appeared in every numbered FF game?",
+        imgSrc : "../images/cloud-seph.gif", //final fantasy
+        choices: ["Cid", "Sid", "Syd","Cloud"],
+        correctAnswer: "Cid"
+    },
+    {
+        question : "When was the first Super Smash Bros released?",
+        imgSrc : "../images/smash-bros.gif", //smash bros
+        choices: ["1998", "2002", "1997", "1999"],
+        correctAnswer: "1999"
+    },
+    {
+        question : "What name did the protaginists call their team?",
+        imgSrc : "../images/persona-5.gif", //persona
+        choices: ["The Crew","The Thieves", "The Phantom Thieves", "The Heart Seekers"],
+        correctAnswer: "The Phantom Thieves"
+    },
+    {
+        question : "In most Silent Hill games there is a 'joke' ending. What is it called?",
+        imgSrc : "../images/robbie-silenthill.gif", //silent hill
+        choices: ["The Joke Ending", "The Cat Ending", "The Dog Ending", "The Funny Ending"],
+        correctAnswer: "The Dog Ending"
+    },
+    
+];
