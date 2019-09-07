2e1df2cc617055cafb1cc0f0684b1d62f27c293c
Julia graves <gravehaus@gmail.com>
2019-09-06 20:08:36 -0700

    got timer to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,74 @@
+//Display question and choices in browser
+
 //initial values
 let counter = 10;
 let currentQuestion = 0;
-let score =0;
+let score = 0;
 let lost = 0;
 let timer;
 
+function nextQuestion() {
+    const isQuestionOver = (quizQuestions.length -1) === currentQuestion;
+    if (isQuestionOver) {
+        console.log('game over');
 
+    } else {
+        currentQuestion++;
+        loadQuestion();
+    }
 
-
-function loadQuestion(){
-    const question= quizQuestions[currentQuestion].question;
-    const choices= quizQuestions[currentQuestion].choices;
-    //$('#time').html('Timer ' + counter);
-    //$('#game').html('<h4>' + question + '</h4>')
-    $('#time').html(counter);
-    $('#game').html('
-    <h4>${question}</h4>'
-
-    );
-
-    
-    
 }
 
-function loadChoices(choices){
-    let result = '';
-    for (let i = 0; i < choices.length; i ++){
-        result += '<p class = "choice" data-answer="${choices[i]}">${choices[i]}></p>';
+    //Make timer
+    function timeUp() {
+        clearInterval(timer);
+
+        lost++;
+
+        nextQuestion();
+    }
+
+    function countDown() {
+        counter--;
+
+        $('#time').html('Timer: ' + counter);
+        if (counter === 0) {
+            timeUp();
+        }
 
     }
-    return result;
-}
 
-loadQuestion();
+    function loadQuestion() {
+
+        counter = 10;
+        timer = setInterval(countDown, 1000);
+
+
+
+
+        const question = quizQuestions[currentQuestion].question;
+        const choices = quizQuestions[currentQuestion].choices;
+        $('#time').html('Timer ' + counter);
+        $('#game').html(`
+    <h4>${question}</h4>
+    ${loadChoices(choices)}
+        
+    `);
+
+    }
+
+    function loadChoices(choices) {
+        let result = '';
+        for (let i = 0; i < choices.length; i++) {
+            result += `<p class = "choice" data-answer="${choices[i]}">${choices[i]}></p>`;
+
+        }
+        return result;
+    }
+
+    //go to next question whether right or wrong
+
+    loadQuestion();
 
 
 
